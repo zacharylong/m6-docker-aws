7279b148f484d734f7a05c860aa43dbc05b3ede3
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-07-04 22:49:13 -0400

    fix nginx in EC2 auto boot script

--- ec2-docker-setup.sh
+++ ec2-docker-setup.sh
@@ -21,26 +21,37 @@
 # Install packages
 yum -y update
 yum install -y emacs-nox nano tree python3 git postgresql postgresql-devel gcc python3-devel
+amazon-linux-extras install -y nginx1
 pip3 install --user boto3 psycopg2 flask uwsgi 
+
 
 # Configure/install custom software
 # Deploy python image gallery
 cd /home/ec2-user
 git clone https://github.com/zacharylong/python-image-gallery-m6.git
 chown -R ec2-user:ec2-user python-image-gallery-m6
-su ec2-user -c "cd ~/python-image-gallery-m6 && pip3 install -r requirements.txt --user"
+su ec2-user -l -c "cd ~/python-image-gallery-m6 && pip3 install -r requirements.txt --user"
 
 BUCKET="zacs-m6-image-gallery-config"
 
+#copy script files into S3
 cd /home/ec2-user/python-image-gallery-m6
 
 aws s3 cp ec2-scripts/ec2-prod-1.1.sh s3://${BUCKET}
 aws s3 cp ec2-scripts/ec2-prod-latest.sh s3://${BUCKET}
 aws s3 sync nginx s3://${BUCKET}/nginx
 
+#configure nginx
+aws s3 cp s3://${CONFIG_BUCKET}/nginx/nginx.conf /etc/nginx
+aws s3 cp s3://${CONFIG_BUCKET}/nginx/default.d/image_gallery.conf /etc/nginx/default.d
+aws s3 cp s3://${CONFIG_BUCKET}/nginx/index.html /usr/share/nginx/html
+chown nginx:nginx /usr/share/nginx/html/index.html
+
 # Start/enable services
 systemctl stop postfix
 systemctl disable postfix
+systemctl start nginx
+systemctl enable nginx
 
 #configure docker for aws
 cd /home/ec2-user
@@ -50,6 +61,8 @@ usermod -a -G docker ec2-user
 
 #need to logout here to pick up groups?
 
+#auto-start nginx server in background
+# su ec2-user -l -c "cd ~/python-image-gallery && ./start" >/var/log/image_gallery.log 2>&1 &
 
 #m6 docker
 git clone https://github.com/zacharylong/m6-docker-aws.git
