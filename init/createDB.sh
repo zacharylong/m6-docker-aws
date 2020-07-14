48fd05e985a08a6e10085d27786e474533f4eb39
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-07-14 19:50:19 -0400

    clean up config files and createDB

--- init/createDB.sh
+++ init/createDB.sh
@@ -4,6 +4,6 @@
 #create user image_gallery login password 'simple';
 #create database image_gallery owner image_gallery;
 
-psql -d postgresql://$IG_USER:$IG_PASSWD@$PG_HOST:$PG_PORT/$IG_DATABASE create user image_gallery login password 'simple';
+psql -d postgresql://$IG_USER:$IG_PASSWD@$PG_HOST:$PG_PORT/$IG_DATABASE create user image_gallery login password 'n,|gRz$#_Bc&EmAjyI)t[j3vCv^4ty4n';
 psql -d postgresql://$IG_USER:$IG_PASSWD@$PG_HOST:$PG_PORT/$IG_DATABASE create database image_gallery owner image_gallery;
 psql -d postgresql://$IG_USER:$IG_PASSWD@$PG_HOST:$PG_PORT/$IG_DATABASE -f create_tables.sql
